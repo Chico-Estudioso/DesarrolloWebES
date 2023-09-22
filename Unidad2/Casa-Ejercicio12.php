77a19be0458a58ecab5bc4722924490aaabb791f
Chico-Estudioso <mauroserrano01@educarex.es>
2023-09-22 13:18:29 +0200

    Version 1

--- Unidad2/Casa-Ejercicio12.php
+++ Unidad2/Casa-Ejercicio12.php
@@ -1,63 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ejercicio12</title>
+    <style>
+        table,tr,td{
+            border-collapse: collapse;
+            border: 1px solid black;
+            text-align: left,center;
+            padding-top: 7px;
+            padding-bottom: 7px;
+            padding-right: 30px;
+        }
+        #tabla{
+            border-collapse: collapse;
+            border: 1px solid black;
+            text-align: center;
+        }
+        #celda{
+            padding: 5px;
+        }
+        #cd1,#cd2{
+            text-align: center;
+            padding: 5px;
+        }
+        
+    </style>
 </head>
-<--Crea un array como el que se muestra a continuación. Recorrer el array y muestra de qué tipo es cada celda del mismo. Para el array asociativo, se debe mostrar todo su contenido (Usa la instrucción foreach para recorres este tipo de array): !>
-
-    <body>
+<body>
+    <h1>EJERCICIO 12-MAURO SERRANO</h1>
+    <?php
+    $datosPersona = array();
+    $datosPersona[]=2345.65;
+    $datosPersona[]="Carlos";
+    $datosPersona[]=34;
+    $datosPersona[]=array('Nombre'=>"María",
+                                    'Edad'=>19);
+    $datosPersona['boolean']='True';
+    ?>
+    <table>
         <?php
-        echo '<h1> Ejercicio 12 </h1>';
-        $censo = array();
-        $hija = array();
-        echo '<table>';
-        echo '<tr>';
-        echo '<td>';
-        $censo[0] = 2345.65;
-        echo '</td>';
-        echo '</tr>';
-        echo '<tr>';
-        echo '<td>';
-        $censo[1] = 'Carlos';
-        echo '</td>';
-        echo '</tr>';
-        echo '<tr>';
-        echo '<td>';
-        $censo[2] = 34;
-        echo '</td>';
-        echo '</tr>';
-        echo '<tr>';
-        echo '<td>';
-        echo '<table>';
-        echo '<tr>';
-        echo '<td>';
-            echo 'Nombre';
-        echo '</td>';
-        echo '<td>';
-            echo 'Edad';
-        echo '</td>';
-        echo '</tr>';
-        echo '<tr>';
-        echo '<td>';
-            echo '"María"';
-        echo '</td>';
-        echo '<td>';
-            echo '19';
-        echo '</td>';
-        echo '</tr>';
-        echo '</table>';
-        echo '</td>';
-        echo '</tr>';
-        echo '<tr>';
-        echo '<td>';
-            $censo[3]=true;
-        echo '</td>';
-        echo '</tr>';
-        echo '</table>';
-        ?>
-    </body>
+        foreach($datosPersona as $valor){
+        
+            if(is_array($valor)){
+                echo '<td id="celda">';
+                echo '<table id="tabla">';
+                foreach($valor as $indice2=>$valor2){
+                    echo '<tr>';
+                    echo '<td id="cd1">'.$indice2.'</td>';
+                    echo '<td id="cd2">'.$valor2.'</td>';
+                    echo '</tr>';
+                }
+                echo '</td>';
+                echo '</table>';
 
+            }else{
+                echo '<td>'.$valor.'</td>';
+            }
+        }
+        ?>
+    </table>
+        <?php
+            echo '<br>';
+            $num=0;
+            foreach($datosPersona as $indice=>$var){
+        
+             if(is_array($var)){
+                 foreach($var as $ind2=>$var2){
+                     echo '=>'.$ind2.' '.$var2.'<br>';
+              }
+             }else{
+             echo 'Posicion'.$num.' '.$indice.' - Valor '.$var.'<br>';
+             }
+          }
+        ?>
+</body>
 </html>
