91fd994f4692bfdb7ab9958d2c36757fd90511c0
Chico-Estudioso <mauroserrano01@educarex.es>
2023-09-25 10:09:05 +0200

    Clase 7

---
+++ ejercicio14_tratamiento.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina</title>
+</head>
+<body>
+    <?php
+        echo '<h1>TRATAMEINTO DE FORMULARIO EJERCICIO 14</h1>';
+    ?>
+</body>
+</html>
