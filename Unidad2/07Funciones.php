5b4cf0f2b3a335aeb31fc6c10cd80f23576cc380
Chico-Estudioso <mauroserrano01@educarex.es>
2023-09-25 08:52:15 +0200

    Clase 7

--- Unidad2/07Funciones.php
+++ Unidad2/07Funciones.php
@@ -1,6 +1,13 @@
 <?php
-
+    //Mostrar fecha actual con formato
     echo '<p>Hoy es:'.date('d/m/Y/ H:i').' </p>';
+    //Mostrar fecha actual con formato usando funcion time que devuelve el instante actual
+    echo '<p>Hoy es:'.date('d/m/Y/ H:i', time()).' </p>';
+    //Mostrar el nº segundos desde el 01/01/1970 hasta ahora
+    echo time();
+    //Convertir una fecha a nº y mostrarlo
     echo '<p>Ayer fue:'.date('d/m/Y/ H:i',strtotime('2023-09-21')).' </p>';
+   // SUma 1 día (representado en segundos 24*60*60) al momento actual time()
+    echo "<p>Mañana es: ".date('d/m/Y H:i',time()+(24*60*60))."</p>";
 
 ?>
