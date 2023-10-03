5b013c48b97ea853656631635e7797f3e3e14865
Chico-Estudioso <mauroserrano01@educarex.es>
2023-10-03 10:57:43 +0200

    Clase 15

--- Unidad2/11POO/GestionarAlumno.php
+++ Unidad2/11POO/GestionarAlumno.php
@@ -1,5 +1,6 @@
 <?php
 include_once 'claseAlumno.php';
+include_once 'AccesoDatos.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,31 +13,66 @@ include_once 'claseAlumno.php';
 
 <body>
     <form action="" method="post">
-    <div>
-        <label for="numExp">NºExpediente</label>
-        <input type="text" name="numExp" placeholder="nombre Alumno">
-    </div>
-    <div>
-        <label for="nombre">Nombre</label>
-        <input type="text" name="nombre" >
-    </div>
-    <div>
-        <label for="fechaN">Fecha Nacimiento</label>
-        <input type="date" name="fechaN" >
-    </div>
-
+        <div>
+            <label for="numExp">Nº Expediente</label><br />
+            <input name="numExp" type="number" value="1" />
+        </div>
+        <div>
+            <label for="nombre">Nombre</label><br />
+            <input name="nombre" type="text" required="required" placeholder="Introduce nombre Alumno" />
+        </div>
+        <div>
+            <label for="fechaN">Fecha Nacimiento</label><br />
+            <input name="fechaN" type="date" value="<?php echo date('Y-m-d') ?>" />
+        </div>
+        <input type="submit" name="crear" value="Crear Alumno">
     </form>
     <?php
-        if(isset($_POST['crear'])){
-            $a=new Alumno($_POST['numExp'],$_POST['nombre'],$_POST['fechaN']);
-            $a->mostrar();
+    //Si pulsamos en Crear Alumno se crea el objeto y se muestra
+    if (isset($_POST['crear'])) {
+        $a = new Alumno($_POST['numExp'], $_POST['nombre'], strtotime($_POST['fechaN']));
+        $a->mostrar();
+    }
+
+    //Chequear si el nº de expediente no existe en el fichero y si existe dará error
+    $alTmp = obtenerAlumno($a->getNumExp());
+    if ($alTmp == null) {
+        // Guardar el alumno en el fichero
+        if (crearAlumno($a)) {
+            echo '<p style="color:blue;">Alumno creado: ' . $a->mostrar() . '</p>';
+        } else {
+            echo '<p style="color:blue;">Error al crear alumno</p>';
         }
+    } else {
+        echo '<p style="color:blue;">El alumno ya existe</p>';
+    }
+
+
+    function obtenerAlumno(int $numExp)
+    {
+        $resultado = null;
+        global $nombreFichero;
+        try {
+            $contenido = file($nombreFichero);
+            foreach ($contenido as $linea) {
+                $datos=explode(';',$linea);
+                if ((int)$datos[0]==$numExp) {
+                    $resultado=new Alumno($datos[0],$datos[1],$datos[2]);
+                    return $resultado;
+                }
+            }
+
+        } catch (\Throwable $th) {
+           echo $th->getMessage();
+        }
+        return $resultado;
+    }
     ?>
-    
+
     <!--<?php
-    /*$a = new Alumno(1, 'Paco', time());
+        /*$a = new Alumno(1, 'Paco', time());
     $a->mostrar();*/
-    ?>-->
+        ?>-->
 </body>
 
 </html>
