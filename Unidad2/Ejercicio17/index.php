c41188b6dc5fdcf5f210f91191d1d43bf196f59b
Chico-Estudioso <mauroserrano01@educarex.es>
2023-10-06 12:50:27 +0200

    Clase 19

--- Unidad2/Ejercicio17/index.php
+++ Unidad2/Ejercicio17/index.php
@@ -1,49 +1,74 @@
 <?php
 require_once 'ClaseCita.php';
-require_once 'modelo.php';
+require_once 'Modelo.php';
+//Instaciar la variable $ad como objeto de Modelo
+$ad=new Modelo();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Peluquería</title>
 </head>
-
 <body>
     <form action="" method="post">
         <div>
-            <label for="fecha">Fecha/Hora </label>
-            <input type="date" name="fecha" value="<?php echo date('Y-m-d'); ?>">
-            <input type="time" name="hora" value="<?php echo date('h:i'); ?>">
+            <label for="fecha">Fecha/Hora</label><br/>
+            <input type="date" id="fecha" name="fecha" value="<?php echo date('Y-m-d');?>"/>
+            <input type="time" name="hora" value="<?php echo date('h:i');?>"/>
         </div>
         <div>
-            <label for="nombre">Nombre del cliente </label>
-            <input type="text" name="nombre" placeholder="Nombre deñ Cliente">
+            <label for="nombre">Nombre del cliente</label><br/>
+            <input type="text" id="nombre" name="nombre" placeholder="Nombre del cliente"/>
         </div>
         <div>
-            <label for="tipoS">Selecciona el servicio </label>
-            <select name="tipoS">
+            <label for="tipoS">Selecciona el servicio</label><br/>
+            <select id="tipoS" name="tipoS">                
                 <option value="1">Corte Sra.</option>
                 <option value="2">Corte Sr.</option>
                 <option value="3">Corte Tinte.</option>
-                <option value="4">Corte Mechas.</option>
-
+                <option value="4">Corte Mechas</option>              
             </select>
         </div>
         <div>
-            <input type="submit" name="crear" value="Crear Cita">
-        </div><?php
-                if (isset($_POST['crear'])) {
-                    if (empty($_POST['fecha']) or empty($_POST['hora']) or empty($_POST['nombre']) or empty($_POST['tipoS'])) {
-                        echo '<h3>MAL<\h3>';
-                    } else {
-                        $cita = new Cita(strtotime($_POST['fecha']), strtotime($_POST['hora']), $_POST['nombre'], $_POST['tipoS']);
-                    }
-                }
-                ?>
+            <input type="submit" name="crear" value="Crear Cita"/>
+        </div>        
     </form>
+    <?php
+    //Si se ha pulsado en crearCita, hay que crear la cita en el fichero
+    if(isset($_POST['crear'])){
+        //Chequear que los campos está rellenos
+        if(empty($_POST['fecha']) or empty($_POST['hora']) or empty($_POST['nombre'])
+           or empty($_POST['tipoS'])){
+            echo '<h3 style="color:red">Error, rellena todos los campos</h3>';
+        }
+        else{
+            $cita=new Cita($_POST['fecha'],
+                       $_POST['hora'],
+                       $_POST['nombre'],
+                       $_POST['tipoS']);
+            //No vamos a chequear que la cita exista
+            //Guadar cita en el fichero            
+            if($ad->crearCita($cita)){
+                echo '<h3 style="color:blue">Cita creada</h3>';
+            }
+            else{
+                echo '<h3 style="color:red">Error al crear la cita</h3>';
+            }
+        }
+       //REcargar
+       header('location:index.php'); 
+    }
+    ?>
+    <table width="50%" align="center">
+        <tr>
+            <th>Fecha</th>
+            <th>Hora</th>
+            <th>Cliente</th>
+            <th>Tipo Servicio</th>
+            <th>Duracion</th>
+        </tr>
+    </table>
 </body>
-
 </html>
