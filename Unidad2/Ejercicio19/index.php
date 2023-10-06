4cea66f4b7c555d1315cfdab8036663f23fc2a14
Chico-Estudioso <mauroserrano01@educarex.es>
2023-10-06 14:26:54 +0200

    Clase 19

--- Unidad2/Ejercicio19/index.php
+++ Unidad2/Ejercicio19/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Conocidos con derecho a quedar y tratarlos bien y mal</title>
 </head>
 
 <body>
