2fc712ea682229aa70cfcacfac964dd44344b99e
Chico-Estudioso <mauroserrano01@educarex.es>
2023-10-09 09:02:45 +0200

    Clase 20

--- Unidad2/Ejercicio19/index.php
+++ Unidad2/Ejercicio19/index.php
@@ -17,14 +17,14 @@
     </form>
     <div>
         <?php
-        if (isset($_POST['rellenar'])) {
-        ?>Color Fondo:<input type="color" name="color">
+        if (isset($_POST['rellenar']) or isset($_POST['mostrar'])) {
+        ?>Color Fondo:<input type="color" name="color" value="<?php echo (isset($_POST['colorF']) ? $_POST['colorF'] : ""); ?>">
         <br>
-        Color Texto:<input type="color" name="colorI">
+        Color Texto:<input type="color" name="colorI" value="<?php echo (isset($_POST['colorT']) ? $_POST['colorT'] : ""); ?>">>
         <br>
     <?php
             for ($i = 0; $i < $_POST['numero']; $i++) {
-                echo '<input type="text" name="opciones[]" placeholder="Opción:' . ($i + 1) . '"/>';
+                echo '<input type="text" name="opciones[]" placeholder="Opción:' . ($i + 1) . '" value="'.(isset($_POST['opciones'][$i])?$_POST['opciones'][$i]:"").'"/>';
             }
         }
     ?>
@@ -39,7 +39,7 @@
         if (isset($_POST['opciones'])) {
             foreach ($_POST['opciones'] as $opcion) {
                 echo '<span style="background-color: ' . $_POST['colorF'] .
-                    ';color: ' . $_POST['colorP'] . '; border: 5px;' . $opcion . '></span>';
+                    ';color: ' . $_POST['colorP'] . '; margin: 5px;' . $opcion . '></span>';
             }
         }
     }
