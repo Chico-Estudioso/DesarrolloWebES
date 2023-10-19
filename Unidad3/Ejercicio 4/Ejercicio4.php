156990954a491d58b467fbd5a2334caa9ff2f12f
Chico-Estudioso <mauroserrano01@educarex.es>
2023-10-19 10:21:05 +0200

    19/10 Jueves

--- Unidad3/Ejercicio 4/Ejercicio4.php
+++ Unidad3/Ejercicio 4/Ejercicio4.php
@@ -1,3 +1,35 @@
-<?php 
-    require_once 'Evento.php';
+<?php
+require_once 'Evento.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 4</title>
+</head>
+
+<body>
+    <h1>CALENDARIO DE EVENTOS</h1>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td>Fecha</td>
+                <td>Hora</td>
+                <td>Asunto</td>
+                <td>Acción</td>
+            </tr>
+
+            <tr>
+                <td>Fecha</td>
+                <td>Hora</td>
+                <td>Asunto</td>
+                <td>Acción</td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
