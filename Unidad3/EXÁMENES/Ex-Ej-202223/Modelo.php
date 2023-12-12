34171e0df268c59c06f6116ebdd1affb30273788
Chico-Estudioso <mauroserrano01@educarex.es>
2023-12-12 10:14:01 +0000

    Martes 12/12

--- Unidad3/EXÁMENES/Ex-Ej-202223/Modelo.php
+++ Unidad3/EXÁMENES/Ex-Ej-202223/Modelo.php
@@ -1,53 +1,125 @@
 <?php
 require_once 'Empleado.php';
-
-class Modelo{
-    private string $url='mysql:host=localhost;port=3306;dbname=mensajes';
+require_once 'Departamento.php';
+require_once 'Mensaje.php';
+class Modelo
+{
+    private string $url = 'mysql:host=localhost;port=3306;dbname=mensajes';
     private string $us = 'root';
     private string $ps = '';
 
-    private $conexion=null;
+    private $conexion = null;
 
     function __construct()
     {
-        try{
-            $this->conexion = new PDO($this->url,$this->us,$this->ps);
-        }catch(PDOException $e){
+        try {
+            $this->conexion = new PDO($this->url, $this->us, $this->ps);
+        } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
-    function obtenerEmpleado(int $us){
+    function obtenerEmpleado(int $us)
+    {
         $resultado = null;
-        try{
+        try {
             $consulta = $this->conexion->prepare(
-                'SELECT * from empleado where idEmp = ?');
-            $params=array($us);
-            if($consulta->execute()){
-                if($fila=$consulta->fetch()){
-                    $resultado = new Empleado($fila['idEmp'],
-                    $fila['dni'],$fila['nombreEmp'],
-                    $fila['fechaNac'],$fila['departamento'],
-                    $fila['cambiarPs']);
+                'SELECT * from empleado e join departamento d on e.departamento=d.idDep where idEmp = ?'
+            );
+            $params = array($us);
+            if ($consulta->execute($params)) {
+                if ($fila = $consulta->fetch()) {
+                    $resultado = new Empleado(
+                        $fila['idEmp'],
+                        $fila['dni'],
+                        $fila['nombreEmp'],
+                        $fila['fechaNac'],
+                        new Departamento(
+                            $fila['idDep'],
+                            $fila['nombre']
+                        ),
+                        $fila['cambiarPs']
+                    );
                 }
             }
-        }catch(PDOException $e){
+        } catch (PDOException $e) {
             echo $e->getMessage();
         }
         return $resultado;
     }
-    function login(int $us, string $ps){
+
+    function enviarMensaje(Mensaje $m, $destinatarios)
+    {
+        $resultado = false;
+        try {
+            $this->conexion->beginTransaction();
+            $consulta = $this->conexion->prepare('INSERT into mensaje values(default,?,?,?,curdate(),?');
+            $params = array($m->getDeEmpleado(), $m->getParaDepartamento(), $m->getAsunto(), $m->getMensaje());
+        if ($consulta->execute($params)) {
+            //Hacer un Insert en para, para cada destinatario
+            foreach ($destinatarios as $d) {
+                $consulta = $this->conexion->prepare('INSERT into para values(?,?,false');
+                $params=array(,$d->getIdEmp());
+
+            }
+        }
+        } catch (PDOException $e) {
+            $e->getMessage();
+        }
+    }
+
+    function obtenerEmpleadosDepartamento($idDep)
+    {
+        $resultado = array();
+        try {
+            $consulta = $this->conexion->prepare('SELECT * from empleado where departamento=?');
+            $params = array($idDep);
+            if ($consulta->execute($params)) {
+                while ($fila = $consulta->fetch()) {
+                    $resultado[] = new Empleado(
+                        $fila['idEmp'],
+                        $fila['dni'],
+                        $fila['nombre'],
+                        $fila['fechaNac'],
+                        $fila['departamento'],
+                        $fila['cambiarPs']
+                    );
+                }
+            }
+        } catch (PDOException $e) {
+            $e->getMessage();
+        }
+        return $resultado;
+    }
+
+    function obtenerDepartamentos()
+    {
+        $resultado = array();
+        try {
+            $consulta = $this->conexion->query('SELECT * from departamento order by nombre');
+            if ($consulta->execute()) {
+                while ($fila = $consulta->fetch()) {
+                    $resultado[] = new Departamento(($fila['idDep']), $fila['nombre']);
+                }
+            }
+        } catch (\PDOException $e) {
+            $e->getMessage();
+        }
+        return $resultado;
+    }
+    function login(int $us, string $ps)
+    {
         $resultado = 0;
-        try{
+        try {
             //Ejecutar fucnión almacenada en bd
             $consulta = $this->conexion->prepare('select login(?,?)');
-            $params = array($us,$ps);
-            if($consulta->execute($params)){
-                if($fila=$consulta->fetch()){
+            $params = array($us, $ps);
+            if ($consulta->execute($params)) {
+                if ($fila = $consulta->fetch()) {
                     //devolver lo que devuelve login
                     return $fila[0];
                 }
             }
-        }catch(PDOException $e){
+        } catch (PDOException $e) {
             echo $e->getMessage();
         }
         return $resultado;
--- Unidad3/EXÁMENES/Ex-Ej-202223/mensajes.php
+++ Unidad3/EXÁMENES/Ex-Ej-202223/mensajes.php
@@ -1,72 +1,106 @@
 <?php
-	session_start();
-	if (isset($_SESSION['usuario'])) {
-		//Hay empleado conectado
-		$empleado=$_SESSION['usuario'];
+require_once 'Modelo.php';
+$bd = new Modelo();
+if ($bd->getConexion() == null) {
+	$mensaje = 'Error, ho hay consexión con la bd';
+}
 
-	}else {
-		//Asi rediriges a donde quieres que vaya sino está logueado
-		header('location:login.php');
+session_start();
+if (isset($_POST['cerrar'])) {
+	session_destroy();
+	header('location:login.php');
+}
+if (isset($_SESSION['usuario'])) {
+	//Hay empleado conectado
+	$empleado = $_SESSION['usuario'];
+} else {
+	//Asi rediriges a donde quieres que vaya sino está logueado
+	header('location:login.php');
+}
+if (isset($_POST['enviar'])) {
+	if (empty($_POST['asunto']) or empty($_POST['mensaje'])) {
+		$mensaje = 'Error, rellena asunto y mensaje';
+	} else {
+		$m=new Mensaje(0,$empleado->getIdEmp(),$_POST['para'],$_POST['asunto'], date('Y-m-d'), $_POST['mensaje']);
+		$destinatarios=$bd->obtenerEmpleadosDepartamento($_POST['para']);
+		if ($bd->enviarMensaje($m, $destinatarios)) {
+			$mensaje = 'Mensaje enviado con Éxito!';
+		} else {
+			$mensaje = 'Mensaje no enviado';
+		}
 	}
+}
+
 ?>
 
 <!doctype html>
 <html>
-      <head>
-        <meta charset="utf-8">        
-        <title>Examen 22_23</title>
-       </head>
-     <body>
-     		
-     		<div> 
-                <h1 style='color:red;'>Mensaje si es necesario</h1> 
-            </div>    
-        	<form action="mensajes.php" method="post">              	
-            		<h1 style="color:blue;">Nuevo Mensaje</h1> 
-            		<h2 style="color:blue;"><?php  
-						echo 'Nombre: '.$empleado->getNombre().' -'.' DNI: '.$empleado->getDni().' - Departamento: '.$empleado->getDepartamento();
-					?></h2>             		
-            		<hr/> 
-            		<div> 
-                		<label for="para">Para</label><br/>           		
-                        <select id="para" name="para">
-                        </select>  
-                    </div>  
-            		<div> 
-                		<label for="asunto">Asunto</label><br/>           		
-                        <input type="text" id="asunto" name="asunto" size="50" maxlength="50"/>  
-                    </div>
-                    <div> 
-                		<label for="mensaje">Mensaje</label><br/>           		
-                        <input type="text" id="mensaje" name="mensaje"  size="100" maxlength="100"/>  
-                    </div>                               
-                    <br/><button type="submit" name="Enviar">Enviar</button>
-                    <button type="submit" name="cerrar">Cerrar Sesión</button>
-                    <hr/> 
-            		<h1 style="color:blue;">Bandeja de Entrada</h1> 
-            		<hr/>   
-            		<table width="100%">
-            			<tr>
-            				<th align="left">Id</th>
-            				<th align="left">De</th>
-            				<th align="left">Para Departamento</th>
-            				<th align="left">Fecha</th>
-            				<th align="left">Asunto</th>
-            				<th align="left">Mensaje</th>
-            			</tr>
-            		</table>
-            		<h1 style="color:blue;">Bandeja de Salida</h1> 
-            		<hr/>   
-            		<table width="100%">
-            			<tr>
-            				<th align="left">Id</th>
-            				<th align="left">Para</th>            				
-            				<th align="left">Fecha</th>
-            				<th align="left">Asunto</th>
-            				<th align="left">Mensaje</th>
-            			</tr>
-            		</table>                              
-      		</form>     
-		      
-	</body>
+
+<head>
+	<meta charset="utf-8">
+	<title>Examen 22_23</title>
+</head>
+
+<body>
+
+	<div>
+		<h1 style='color:red;'><?php
+								echo isset($mensaje) ? $mensaje : '';
+								?></h1>
+	</div>
+	<form action="mensajes.php" method="post">
+		<h1 style="color:blue;">Nuevo Mensaje</h1>
+		<h2 style="color:blue;"><?php
+								echo 'Nombre: ' . $empleado->getNombre() . ' -' . ' DNI: ' . $empleado->getDni() . ' - Departamento: ' . $empleado->getDepartamento()->getNombre();
+								?></h2>
+		<hr />
+		<div>
+			<label for="para">Para</label><br />
+			<select id="para" name="para">
+				<?php
+				$departamentos = $bd->obtenerDepartamentos();
+				foreach ($departamentos as $d) {
+					echo '<option value="' . $d->getIdDep() . '">' . $d->getNombre() . '</option>';
+				}
+				?>
+			</select>
+		</div>
+		<div>
+			<label for="asunto">Asunto</label><br />
+			<input type="text" id="asunto" name="asunto" size="50" maxlength="50" />
+		</div>
+		<div>
+			<label for="mensaje">Mensaje</label><br />
+			<input type="text" id="mensaje" name="mensaje" size="100" maxlength="100" />
+		</div>
+		<br /><button type="submit" name="Enviar">Enviar</button>
+		<button type="submit" name="cerrar">Cerrar Sesión</button>
+		<hr />
+		<h1 style="color:blue;">Bandeja de Entrada</h1>
+		<hr />
+		<table width="100%">
+			<tr>
+				<th align="left">Id</th>
+				<th align="left">De</th>
+				<th align="left">Para Departamento</th>
+				<th align="left">Fecha</th>
+				<th align="left">Asunto</th>
+				<th align="left">Mensaje</th>
+			</tr>
+		</table>
+		<h1 style="color:blue;">Bandeja de Salida</h1>
+		<hr />
+		<table width="100%">
+			<tr>
+				<th align="left">Id</th>
+				<th align="left">Para</th>
+				<th align="left">Fecha</th>
+				<th align="left">Asunto</th>
+				<th align="left">Mensaje</th>
+			</tr>
+		</table>
+	</form>
+
+</body>
+
 </html>
