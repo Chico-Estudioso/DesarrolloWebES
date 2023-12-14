4574bce0e6d7861d4938e8640af97e532642938c
Chico-Estudioso <mauroserrano01@educarex.es>
2023-12-14 16:03:34 +0100

    a

--- Unidad3/EXÁMENES/Ex-Ej-202223/login.php
+++ Unidad3/EXÁMENES/Ex-Ej-202223/login.php
@@ -32,27 +32,30 @@
 ?>
 <!doctype html>
 <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Examen 22_23</title>
-       </head>
-     <body>     	
- 			<div> 
-                <h1 style='color:red;'>
-                <?php echo isset($mensaje)?$mensaje:'';?>
-            </h1> 
-            </div>    
-        	<form action="login.php" method="post">              	
-            		<h1>Login</h1>    
-            		<div> 
-                		<label for="usuario">Id de Empleado</label><br/>           		
-                        <input type="text" id="usuario" name="usuario"/>  
-                    </div>
-                    <div> 
-                        <label for="ps">Contraseña</label><br/>                           
-                        <input type="password" id="ps"   name="ps"/>      
-                    </div>                               
-                    <br/><button type="submit" name="acceder">Acceder</button>                        
-      		</form>           
-	</body>
+
+<head>
+    <meta charset="utf-8">
+    <title>Examen 22_23</title>
+</head>
+
+<body>
+    <div>
+        <h1 style='color:red;'>
+            <?php echo isset($mensaje)?$mensaje:'';?>
+        </h1>
+    </div>
+    <form action="login.php" method="post">
+        <h1>Login</h1>
+        <div>
+            <label for="usuario">Id de Empleado</label><br />
+            <input type="text" id="usuario" name="usuario" />
+        </div>
+        <div>
+            <label for="ps">Contraseña</label><br />
+            <input type="password" id="ps" name="ps" />
+        </div>
+        <br /><button type="submit" name="acceder">Acceder</button>
+    </form>
+</body>
+
 </html>
