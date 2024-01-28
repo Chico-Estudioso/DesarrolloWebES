92272053a110a472ceae5307a9cd3dc5d7e362ec
Chico-Estudioso <mauroserrano01@educarex.es>
2024-01-28 18:54:12 +0000

    Commiu

---
+++ EXAMEN-RECU-EJ/ficherosRecuperacion/Modelo.php
@@ -0,0 +1,65 @@
+<?php
+require_once 'Cliente.php';
+require_once 'misActividades.php';
+require_once 'Usuario.php';
+require_once 'Activadad.php';
+require_once 'login.php';
+require_once 'crearCliente.php';
+
+class Modelo
+{
+    private $url = "mysql:host=localhost;port=3306;dbname=gimnasio";
+    private $us = "root";
+    private $ps = "";
+    private $conexion = null;
+
+    function __construct()
+    {
+        try {
+            $this->conexion = new PDO($this->url, $this->us, $this->ps);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    /*
+    DUDAS:
+    1) El $consulta->execute params porque hay veces que va en el if y otras veces que va fuera del inf
+    2) El puntero del fetch lo tenemos que usar en todos los métodos que no nos devuelvan solo 1 resultado
+    o hay que usarlos en todos
+    */
+
+    function obtenerUsuario(string $us, string $ps)
+    {
+        $resultado = 0;
+        try {
+            //Ejecutar fucnión almacenada en bd
+            $consulta = $this->conexion->prepare('SELECT * from usuario where usuario=? and clave=sha(?,0)');
+            $params = array($us, $ps);
+            $consulta->execute($params);
+
+            if ($fila = $consulta->fetch()) {
+                $resultado = new Usuario($fila['usuario'], $fila['tipo']);
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
+
+    function obtenerCliente(string $nombreUsuario)
+    {
+        $resultado = null;
+        try {
+            $consulta = $this->conexion->prepare('SELECT * from cliente where usuario=?');
+            $params = array($nombreUsuario);
+            $consulta->execute($params);
+            if ($fila = $consulta->fetch()) {
+                $resultado = new Cliente($fila['id'], $fila['usuario'], $fila['dni'], $fila['apellidos'], $fila['nombre'], $fila['telf'], $fila['baja']);
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
+}
---
+++ EXAMEN-RECU-EJ/ficherosRecuperacion/login.php
@@ -0,0 +1,59 @@
+<?php
+require_once 'Modelo.php';
+require_once 'Usuario.php';
+require_once 'Cliente.php';
+$bd = new Modelo();
+
+if ($bd->getConexion() == null) {
+    $mensaje = 'Error, no hay conexión con la bd';
+} else {
+    if (isset($_POST['acceder'])) {
+        if (empty($_POST['usuario']) or empty($_POST['ps'])) {
+            $mensaje = 'Error, rellena us y ps';
+        } else {
+            $user = $bd->obtenerUsuario($_POST['usuario'], $_POST['ps']);
+            if ($user != null) {
+                session_start();
+                $_SESSION['usuario'] = $user;
+                if ($user->getTipo() == 'A') {
+                    header("location:crearCliente.php");
+                } elseif ($user->getTipo() == 'C') {
+                    $cliente = $bd->obtenerCliente($user->getUsuario());
+                }
+            } else {
+                $mensaje = 'Error, usuario no encontrado o contraseña errónea';
+            }
+        }
+    }
+}
+?>
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Recuperaci�n T3 22_23</title>
+</head>
+
+<body>
+    <div>
+        <h1 style='color:red;'>
+            <?php echo isset($mensaje) ? $mensaje : ''; ?>
+        </h1>
+    </div>
+    <form action="login.php" method="post">
+        <h1>SuperGim</h1>
+        <div>
+            <label for="usuario">Usuario</label><br />
+            <input type="text" id="usuario" name="usuario" />
+        </div>
+        <div>
+            <label for="ps">Contraseña</label><br />
+            <input type="password" id="ps" name="ps" />
+        </div>
+        <br /><button type="submit" name="acceder">Acceder</button>
+    </form>
+</body>
+
+</html>
